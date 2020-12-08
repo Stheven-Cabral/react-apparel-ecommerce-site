f69a9b3c42c841ffc1ef5b35fc0eb9c5e92a12d0
Stheven Cabral <stheven.cabral@gmail.com>
2020-12-07 21:27:43 -0800

    implement firebase utils, include ability to store authenticated user into our firestore database.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -22,10 +22,28 @@ class App extends React.Component {
   componentDidMount() {
     // The onAuthStateChanged method from auth automatically updates authentication when user changes.
     // It also keeps the user logged in. The subscription below to auth is always open so it needs to be closed.
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        // onSnapshot is a reference method in order to get a view of the data.
+        userRef.onSnapshot(snapShot => {
+          // .data() allows the data to come in as an object and not unusable data.
+          // console.log(snapShot.data());
 
-      console.log(user);
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              // spread operator automatically concatenates the object to the current state.
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      // The below is triggered when userAuth is null from auth.onAuthStateChanged
+      this.setState({ currentUser: userAuth });
     });
   }
   componentWillUnmount() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -18,6 +18,39 @@ const config = {
   measurementId: "G-FTGRQ28KZ0"
 };
 
+// createUserProfileDocument will be used in storing user in our firestore database.
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  // if user is null return nothing.
+  if (!userAuth) return;
+
+  // A reference is a pointer to the requested object or collection.
+  const userRef = (firestore.doc(`users/${userAuth.uid}`));
+
+  // The following snapShot gives you a view of the colllection or object, but you can't use CRUD operattions on it. You have have to use the reference if you want to perform CRUD.
+  const snapShot = await userRef.get();
+
+  // exists is a property on a snapshot that lets us know if a snapshot object exists.
+  if(!snapShot.exists) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        // passing in any additional data as an object to userAuth
+        ...additionalData
+      })
+    } catch(error) {
+      console.log('error creating user', error.message)
+    }
+  }
+
+  // We still might need the userRef for something so we need to return it. 
+  return userRef;
+}
+
 firebase.initializeApp(config);
 
 // auth and firestore are imported from above.
